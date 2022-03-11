5739df6ad446a79bd281a8443fec8447da8ac62c
SoftThunder <grom1304@gmail.com>
2022-03-11 16:41:17 +0300

    задание 4

---
+++ 4.js
@@ -0,0 +1,37 @@
+/**
+ *  функция выполняет сложение
+ * @param {number} x
+ * @param {number} y
+ */
+function addition(x, y) {
+    return x + y;
+}
+
+alert(addition(2, 3));
+/**
+ *  функция выполняет вычитание
+ * @param {number} x
+ * @param {number} y
+ */
+function subtraction(x, y) {
+    return x - y;
+}
+alert(subtraction(2, 3));
+/**
+ *  функция выполняет умножение
+ * @param {number} x
+ * @param {number} y
+ */
+ function multiplication(x, y) {
+    return x * y;
+}
+alert(multiplication(2, 3));
+/**
+ *  функция выполняет деление
+ * @param {number} x
+ * @param {number} y
+ */
+ function division(x, y) {
+    return x / y;
+}
+alert(division(2, 3));
